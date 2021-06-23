516e47dc84129259da416c539a0affb851b25570
Brandon Copeland <copelandbrandon2@gmail.com>
2021-06-23 21:23:41 +0000

    add routing, view and controller for about page

--- config/routes.rb
+++ config/routes.rb
@@ -11,11 +11,13 @@ Rails.application.routes.draw do
   end
 
   resources :orders, only: [:create, :show]
+  resources :about, only: [:index]
 
   namespace :admin do
     root to: 'dashboard#show'
     resources :products, except: [:edit, :update, :show]
   end
+
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
